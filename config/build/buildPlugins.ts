9a2f12462b0a70fc32105686b98d0161c19fd67d
Zayka Julia <zaykajulias@gmail.com>
2023-03-01 10:22:44 +0300

    add webpack bundle analyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,27 +1,28 @@
 import HtmlWebpackPlugin from "html-webpack-plugin"; // плагин для связи вебпака с хтмл файлом, в который будут подключаться скрипты автоматически
 import MiniCssExtractPlugin from "mini-css-extract-plugin"; // плагин для создания css-файлов отдельных в проде
-
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer"; // плагин для анализа размера бандла
 // import path from "path";
 import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 
 // функция для подключения и конфигурации плагинов
 export function buildPlugins({
-    paths,
-    isDev,
+  paths,
+  isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-    return [
-        new HtmlWebpackPlugin({
-            template: paths.html, // указываем, какой файл хтмл мы будет использовать в качестве шаблона  для работы с хтмл
-        }),
-        new webpack.ProgressPlugin(), // для отслеживания прогресса сборки
-        new MiniCssExtractPlugin({
-            filename: "css/[name].[contenthash:8].css",
-            chunkFilename: "css/[name].[contenthash:8].css",
-        }),
-        new webpack.DefinePlugin({
-            __IS_DEV__: JSON.stringify(isDev),
-        }),
-        new webpack.HotModuleReplacementPlugin(), // для обновления страницы без перезагрузки
-    ];
+  return [
+    new HtmlWebpackPlugin({
+      template: paths.html, // указываем, какой файл хтмл мы будет использовать в качестве шаблона  для работы с хтмл
+    }),
+    new webpack.ProgressPlugin(), // для отслеживания прогресса сборки
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[contenthash:8].css",
+      chunkFilename: "css/[name].[contenthash:8].css",
+    }),
+    new webpack.DefinePlugin({
+      __IS_DEV__: JSON.stringify(isDev),
+    }),
+    new webpack.HotModuleReplacementPlugin(), // для обновления страницы без перезагрузки
+    new BundleAnalyzerPlugin({ openAnalyzer: false }),
+  ];
 }
