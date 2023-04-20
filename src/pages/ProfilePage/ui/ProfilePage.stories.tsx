83fad63d8bb3872055afd94a0884fbd91db02389
Zayka Julia <zaykajulias@gmail.com>
2023-04-20 13:09:49 +0300

    add new screenshots, fix stories

--- src/pages/ProfilePage/ui/ProfilePage.stories.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.stories.tsx
@@ -5,6 +5,7 @@ import ProfilePage from "./ProfilePage";
 import "app/styles/index.scss";
 import { ThemeDecorator } from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
 import { Theme } from "app/providers/ThemeProvider";
+import { StoreDecorator } from "shared/config/storybook/StoreDecorator/StoreDecorator";
 
 export default {
   title: "pages/ProfilePage",
@@ -23,8 +24,9 @@ const Template: ComponentStory<typeof ProfilePage> = (args: object) => (
 export const Normal = Template.bind({});
 
 Normal.args = {};
+Normal.decorators = [StoreDecorator({})];
 
 export const Dark = Template.bind({});
 Dark.args = {};
 
-Dark.decorators = [ThemeDecorator(Theme.DARK)];
+Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({})];
