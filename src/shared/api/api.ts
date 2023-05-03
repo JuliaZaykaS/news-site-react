d32d0e5d080d6c8c1c836a6f5a374c74be5b6c24
Zayka Julia <zaykajulias@gmail.com>
2023-05-03 17:55:22 +0300

    fix build storybook

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
 import { USER_LOCALSTORAGE_KEY } from "shared/const/localstorage";
 
-// const baseUrl = __IS_DEV__ ? "http://localhost:8000" : "http://production.ru";
+const baseUrl = __IS_DEV__ ? "http://localhost:8000" : "http://production.ru";
 
 export const $api = axios.create({
-  //   baseURL: baseUrl,
-  baseURL: __API__,
+  baseURL: baseUrl,
+  // baseURL: __API__,
+  // baseURL,
   headers: {
     authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || "",
   },
