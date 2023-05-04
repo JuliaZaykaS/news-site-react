bba06a94f2de71f5b6ac958be123792bd72ab8ac
Zayka Julia <zaykajulias@gmail.com>
2023-05-04 16:14:01 +0300

    screenshots profile page

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -5,6 +5,7 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { ThemeDecorator } from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
 import { Theme } from "app/providers/ThemeProvider";
 import { Sidebar } from "./Sidebar";
+import { StoreDecorator } from "shared/config/storybook/StoreDecorator/StoreDecorator";
 
 export default {
   title: "widgets/Sidebar",
@@ -22,9 +23,20 @@ const Template: ComponentStory<typeof Sidebar> = (args) => (
 export const Light = Template.bind({});
 
 Light.args = {};
-Light.decorators = [ThemeDecorator(Theme.LIGHT)];
+Light.decorators = [
+  ThemeDecorator(Theme.LIGHT),
+  StoreDecorator({ user: { authData: {} } }),
+];
 
 export const Dark = Template.bind({});
 
 Dark.args = {};
-Dark.decorators = [ThemeDecorator(Theme.DARK)];
+Dark.decorators = [
+  ThemeDecorator(Theme.DARK),
+  StoreDecorator({ user: { authData: {} } }),
+];
+
+export const NoAuth = Template.bind({});
+
+NoAuth.args = {};
+NoAuth.decorators = [StoreDecorator({ user: {} })];
