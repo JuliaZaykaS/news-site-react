1ac9cd56b2fb1ed91b9e1d58bb344db25173dcad
Zayka Julia <zaykajulias@gmail.com>
2023-05-10 14:51:11 +0300

    fix profilecard screenshots

--- src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
@@ -9,7 +9,9 @@ import { StoreDecorator } from "shared/config/storybook/StoreDecorator/StoreDeco
 import { Currency } from "entities/Currency";
 import { Country } from "entities/Country";
 import { ProfileCard } from "./ProfileCard";
-import avatar from "shared/assets/tests/example.png";
+// import avatar from "shared/assets/tests/example.png";
+const avatar =
+  "https://pic.rutubelist.ru/user/3b/27/3b2758ad5492a76b578f7ee072e4e894.jpg";
 
 export default {
   title: "entities/ProfileCard",
