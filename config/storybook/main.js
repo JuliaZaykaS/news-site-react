40d219bdb2c68988b767f1d937c2b4a7ebe604f2
Zayka Julia <zaykajulias@gmail.com>
2024-01-15 16:26:30 +0300

    storybook themes addon

--- config/storybook/main.js
+++ config/storybook/main.js
@@ -2,13 +2,21 @@ module.exports = {
   stories: ["../../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        backgrounds: false, // 👈 disable the backgrounds addon
+      },
+    },
     "@storybook/addon-interactions",
     "storybook-addon-mock/register",
+    "storybook-addon-themes",
+
   ],
   framework: "@storybook/react",
   // framework: "@storybook/react-webpack5",
   core: {
     builder: "@storybook/builder-webpack5",
   },
+
 };
