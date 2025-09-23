d784e0e8c460c48e55657e2ddb589c5dffc27eac
Zayka Julia <zaykajulias@gmail.com>
2025-09-23 17:50:59 +0300

    fix test env

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.async.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.async.tsx
@@ -1,10 +1,10 @@
 import { lazy } from 'react';
 import ArticlesPage from './ArticlesPage';
 
-const isTest = __VITE_TEST__;
-export const ArticlesPageAsync = isTest
-    ? ArticlesPage // статический import для тестов
-    : lazy(() => import('./ArticlesPage'));
+// const isTest = __VITE_TEST__ ?? false;
+// export const ArticlesPageAsync = isTest
+//     ? ArticlesPage // статический import для тестов
+//     : lazy(() => import('./ArticlesPage'));
 
-// export const ArticlesPageAsync = lazy(() => import("./ArticlesPage"));
-// export default ArticlesPage
+export const ArticlesPageAsync = lazy(() => import('./ArticlesPage'));
+export default ArticlesPage;
